5faba040c1fcaa33e48b8f123a697a92db154dae
Vadym Samkov <vadym.samkov@itrident.agency>
2026-01-08 15:54:33 +0200

    Removed publishing from core

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -14,16 +14,3 @@ kotlin {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
-                groupId = "com.github.sq-dsl"
-                artifactId = "core"
-                version = "0.0.4"
-            }
-        }
-    }
-}
